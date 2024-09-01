400fca6d4902d69db6a8ee40dd26cecb6c861ad2
Sahil Yadav <97095115+realSahilYadav@users.noreply.github.com>
2024-09-01 13:34:50 +0530

    Learnt loops and patterns

---
+++ primeNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    if (n%2==0)
+    {
+        cout<<n<<" is prime"<<endl;
+    }
+    else
+    {
+        cout<<n<<" is not prime"<<endl;
+    }
+    return 0;
+}
---
+++ sumofN.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n:"<<endl;
+    cin>>n;
+    cout<<"Sum is "<<n*(n+1)/2;
+    return 0;
+}
