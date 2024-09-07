4489b2c5a8595f4cda59c2f74be10dbfd8c677fc
Sahil Yadav <97095115+realSahilYadav@users.noreply.github.com>
2024-09-07 22:21:23 +0530

    Learnt Binary Search

---
+++ binarysearch.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch (int arr[], int size, int key)
+{
+    int start=0, end=size-1;
+    int mid=(start+end)/2;
+
+    while (start<=end)
+    {
+        if (arr[mid]==key)
+        {
+            return mid;
+        }
+        if (key>arr[mid])
+        {
+            start=mid+1;
+        }
+        else
+        {
+            end=mid-1;
+        }
+        
+        mid=(start+end)/2;
+    }
+    return -1;
+}
+
+int main ()
+{
+    int series[8]={12, 18, 19, 28, 36, 42, 44, 56};
+
+    int index=binarySearch(series, 8, 12);
+
+    cout<<"Index of 12 is "<<index<<endl;
+    return 0;
+}
