7df3f07bdc3b4925e7a3b5a6489c319131df9052
Sahil Yadav <97095115+realSahilYadav@users.noreply.github.com>
2024-09-07 22:30:27 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int binarySearch (int arr[], int size, int key)
 {
     int start=0, end=size-1;
-    int mid=(start+end)/2;
+    int mid=start+(end-start)/2;
 
     while (start<=end)
     {
@@ -21,7 +21,7 @@ int binarySearch (int arr[], int size, int key)
             end=mid-1;
         }
         
-        mid=(start+end)/2;
+        mid=start+(end-start)/2
     }
     return -1;
 }
