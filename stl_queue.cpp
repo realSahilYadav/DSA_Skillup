dbcefe37fd460d8a326cc766b366cffb6c20ea29
Sahil Yadav <97095115+realSahilYadav@users.noreply.github.com>
2024-10-14 20:18:25 +0530

    Learnt Queues in STL

---
+++ stl_queue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main()
+{
+    queue<string> q;
+
+    q.push("Sahil");
+    q.push("Yadav");
+    q.push("Kumar");
+
+    cout<<"Size before pop"<<q.size()<<endl;
+    cout<<"First element-> "<<q.front()<<endl;
+    cout<<"Last element-> "<<q.back()<<endl;
+
+    q.pop();
+    cout<<"First element-> "<<q.front()<<endl;
+    cout<<"Last element-> "<<q.back()<<endl;
+    cout<<"Size after pop-> "<<q.size()<<endl;
+
+    return 0;
+}
