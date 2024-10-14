2801a612b2d71c5c0391c2095a3584d585c748b9
Sahil Yadav <97095115+realSahilYadav@users.noreply.github.com>
2024-10-14 20:16:50 +0530

    Learnt Deque in STL

---
+++ stl_deque.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+int main()
+{
+    deque<int>d;
+    d.push_back(1);
+    d.push_front(2);
+
+    for (int i: d)
+    {
+        cout<<i<<" ";
+    }
+
+    // d.pop_back();
+    // for (int i: d)
+    // {
+    //     cout<<i<<" ";
+    // }
+
+    // d.pop_front();
+    // for(int i: d)
+    // {
+    //     cout<<i<<" ";
+    // }
+
+    cout<<"Element at index 1-> "<<d.at(1)<<endl;
+    cout<<"First element-> "<<d.front();
+    cout<<"Last element-> "<<d.back();
+    cout<<"Empty or not-> "<<d.empty();
+
+    d.clear();
+    for (int i: d)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
