f39f740803f6f36670e98cc019b169bd45437002
Sahil Yadav <97095115+realSahilYadav@users.noreply.github.com>
2024-10-14 20:15:56 +0530

    Learnt Array in STL

---
+++ stl_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main()
+{
+    array<int,4> a={1,2,3,4};
+    int size=a.size();
+    
+    for (int i=0; i<size; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+    cout<<"Element at 3rd index --> "<<a.at(3)<<endl;
+    cout<<"Array empty or not --> "<<a.empty()<<endl;
+    cout<<"First element --> "<<a.front()<<endl;
+    cout<<"Last element --> "<<a.back()<<endl;
+    
+    return 0;
+}
