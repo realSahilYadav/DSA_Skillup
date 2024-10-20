6629506ed4e5b5cf6799ea5d390357ad5e41ea81
realSahilYadav <sahil2006.gg@gmail.com>
2024-10-20 16:56:27 +0530

    Learnt Binary Search

---
+++ squareroot.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int squareRoot(int n)
+{
+    int start = 0;
+    int end = n;
+    int mid = 1;
+    int sq = 1;
+    int target = n;
+    int rem = 1;
+
+    while (start <= end)
+    {
+        mid = start + (end - start)/2;
+        sq = (mid * mid);
+        rem = target / sq;
+
+        if (rem == 1)
+        {
+            return mid;
+        }
+
+        if (rem < 1)
+        {
+            end = mid - 1;
+        }
+
+        if (rem > 1)
+        {
+            start = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter n: "<<endl;
+    cin>>n;
+
+    cout<<"Square root of "<<n<<" is "<<squareRoot(n);
+
+    return 0;
+
+}
