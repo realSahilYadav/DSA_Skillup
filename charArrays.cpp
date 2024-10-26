17a9234a3db920c079aecb26c27f74b7aa31c12e
realSahilYadav <sahil2006.gg@gmail.com>
2024-10-26 08:45:24 +0530

    Learnt about strings

---
+++ charArrays.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[20];
+    cout<<"Enter your name:"<<endl;
+    cin>>name;
+    cout<<endl;
+    cout<<"Your name is:"<<endl<<name;
+
+    return 0;
+
+}
